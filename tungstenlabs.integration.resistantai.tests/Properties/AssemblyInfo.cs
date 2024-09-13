4851306455bc7b0c7fa4fe46f9502a4250b123e0
Joseph Joy <joseph.joy@tungstenautomation.com>
2024-09-13 11:17:10 +1000

    Code refectoring

---
+++ tungstenlabs.integration.resistantai.tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("tungstenlabs.integration.resistantai.tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("tungstenlabs.integration.resistantai.tests")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("7d8f3c57-aefa-4f67-90c4-d51f1010f096")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
