38d54902c8a8b784c1541fb50f6c6b5d24ae4c22
Siva <siva.pingali@tungstenautomation.com>
2025-05-21 16:29:43 -0400

    Updated code to replace sleep and Sync methods with Async ttsaks. Also to add multiple retries to bring tiffKTADocument in DocumentAnalysis class

--- tungstenlabs.integration.resistantai.tests/APIHelperTests.cs
+++ tungstenlabs.integration.resistantai.tests/APIHelperTests.cs
@@ -28,9 +28,9 @@ namespace tungstenlabs.integration.resistantai.tests
         public void GetDocumentWithBoundingBoxes()
         {
             string temp;
-            string docID = DAHelper.GetDocumentWithBoundingBoxes(@"https://eu.id.resistant.ai/oauth2/aus2un1hkrKhPjir4417/v1/token", @"https://api.documents.resistant.ai/v2/submission", @"0oa4oy1pr05JNfPBQ417", 
-                                                                 @"5sC2ip96HaH-mQhBASjZpXcukqaTSQ8BPg91LatX", @"d943bd2f-e2ce-463f-9715-b29901128ae8", @"https://ktacloudeco-dev.ttaprt.dev.tungstencloud.com/Services/Sdk",
-                                                                 @"D2A967C768C7854B91C210DF77F118A4", "ad90a052-b7d4-4897-83d7-f01e281728cc", "content_hiding");
+            string docID = DAHelper.GetDocumentWithBoundingBoxes(Constants.RAI_URL_TOKEN, Constants.RAI_URL_API, Constants.RAI_CLIENT_ID, Constants.RAI_CLIENT_SECRET,
+                                                                    @"d943bd2f-e2ce-463f-9715-b29901128ae8", Constants.TOTALAGILITY_API_URL, Constants.TOTALAGILITY_SESSION_ID, 
+                                                                    "ad90a052-b7d4-4897-83d7-f01e281728cc", "content_hiding");
         }
     }
 }
