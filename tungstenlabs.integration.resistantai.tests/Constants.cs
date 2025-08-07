5483b1a2f8b0a83063339799622547c0fb16b969
Siva <siva.pingali@tungstenautomation.com>
2025-08-07 16:38:14 -0400

    Changes in constants

--- tungstenlabs.integration.resistantai.tests/Constants.cs
+++ tungstenlabs.integration.resistantai.tests/Constants.cs
@@ -2,26 +2,14 @@
 {
     public static class Constants
     {
-        //public const string TOTALAGILITY_API_URL = @"ToBeFilled";
-        //public const string TOTALAGILITY_SESSION_ID = @"ToBeFilled";
+        public const string TOTALAGILITY_API_URL = @"ToBeFilled";
+        public const string TOTALAGILITY_SESSION_ID = @"ToBeFilled";
 
-        //public const string RAI_URL_TOKEN = @"ToBeFilled";
-        //public const string RAI_URL_API = @"ToBeFilled";
-        //public const string RAI_CLIENT_ID = @"ToBeFilled";
-        //public const string RAI_CLIENT_SECRET = @"ToBeFilled";
-
-        public const string TOTALAGILITY_API_URL = @"https://ktacloudeco-dev.ttaprt.dev.tungstencloud.com/Services/Sdk";
-
-        public const string TOTALAGILITY_SESSION_ID = @"D2A967C768C7854B91C210DF77F118A4";
+        public const string RAI_URL_TOKEN = @"ToBeFilled";
+        public const string RAI_URL_API = @"ToBeFilled";
+        public const string RAI_CLIENT_ID = @"ToBeFilled";
+        public const string RAI_CLIENT_SECRET = @"ToBeFilled";
 
 
-
-        public const string RAI_URL_TOKEN = @"https://eu.id.resistant.ai/oauth2/aus2un1hkrKhPjir4417/v1/token";
-
-        public const string RAI_URL_API = @"https://api.documents.resistant.ai/v2/submission";
-
-        public const string RAI_CLIENT_ID = @"0oa4oy1pr05JNfPBQ417";
-
-        public const string RAI_CLIENT_SECRET = @"5sC2ip96HaH-mQhBASjZpXcukqaTSQ8BPg91LatX";
     }
 }
